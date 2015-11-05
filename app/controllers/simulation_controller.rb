31f70fccad6235b5feb2b46c20c5fdbfc786822f
pinzolo <pinzolo@gmail.com>
2015-11-05 15:24:30 +0900

    Implements exec action

--- app/controllers/simulation_controller.rb
+++ app/controllers/simulation_controller.rb
@@ -3,5 +3,15 @@ class SimulationController < ApplicationController
   end
 
   def exec
+    if params[:portal_data]
+      portals = CfSim::DataFileParser.new(params[:portal_data].tempfile.path).parse
+      portal_map = CfSim::PortalMap.new(portals)
+      generator = CfSim::IntelMapUrlGenerator.new(portal_map)
+      finder = CfSim::ControlFieldSetFinder.new(CfSim::PointList.new(portal_map.points).creatable_fields, limit_field_count: 10)
+      fields_list = finder.find_max_count_fields_list
+      max_area = fields_list.first.total_area
+      @field_count = fields_list.first.size
+      @results = fields_list.map { |fields| Result.new(generator.fields_link(fields), (fields.total_area * 100 / max_area).round(3)) }
+    end
   end
 end
