eb1f5682fc6595d1e2835e22a51cd15f0d453274
pinzolo <pinzolo@gmail.com>
2015-11-06 11:02:16 +0900

    処理できないファイルがアップロードされた時の処理を追加

--- app/controllers/simulation_controller.rb
+++ app/controllers/simulation_controller.rb
@@ -3,15 +3,29 @@ class SimulationController < ApplicationController
   end
 
   def exec
+    @error = nil
     if params[:portal_data]
-      portals = CfSim::DataFileParser.new(params[:portal_data].tempfile.path).parse
-      portal_map = CfSim::PortalMap.new(portals)
-      generator = CfSim::IntelMapUrlGenerator.new(portal_map)
-      finder = CfSim::ControlFieldSetFinder.new(CfSim::PointList.new(portal_map.points).creatable_fields, limit_field_count: 10)
-      fields_list = finder.find_max_count_fields_list
-      max_area = fields_list.first.total_area
-      @field_count = fields_list.first.size
-      @results = fields_list.map { |fields| Result.new(generator.fields_link(fields), (fields.total_area * 100 / max_area).round(3)) }
+      begin
+        portals = CfSim::DataFileParser.new(params[:portal_data].tempfile.path).parse
+        portal_map = CfSim::PortalMap.new(portals)
+        generator = CfSim::IntelMapUrlGenerator.new(portal_map)
+        finder = CfSim::ControlFieldSetFinder.new(CfSim::PointList.new(portal_map.points).creatable_fields, limit_field_count: 10)
+        fields_list = finder.find_max_count_fields_list
+        max_area = fields_list.first.total_area
+        @field_count = fields_list.first.size
+        @results = fields_list.map { |fields| Result.new(generator.fields_link(fields), (fields.total_area * 100 / max_area).round(3)) }
+      rescue => e
+        handle_error(e)
+        @error = 'File is invalid. Require CSV(latitude,longitude)'
+        render status: :bad_request
+      end
     end
+  end
+
+  private
+
+  def handle_error(e)
+    Rails.logger.error("#{e.class}: #{e.message}")
+    e.backtrace.each { |b| Rails.logger.error("  #{b}") }
   end
 end
